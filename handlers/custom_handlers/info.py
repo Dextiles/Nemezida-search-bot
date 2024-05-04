f79d2347cd7faaa23a5cac6ed57ba7d516bdff5c
Иван <Dextiles@outlook.com>
2024-05-04 23:48:05 +0300

    fix

--- handlers/custom_handlers/info.py
+++ handlers/custom_handlers/info.py
@@ -6,7 +6,7 @@ from database.db_controller import ParsedDataController as Controller
 @bot.message_handler(commands=["info"])
 def bot_start(message: Message):
     metadata = Controller().get_metadata()
-    bot.reply_to(message, f'<b>Дата обновления БД:</b>\n{metadata[0][0].replace('-', '.')}\n'
+    bot.reply_to(message, f'<b>Дата обновления БД:</b>\n{metadata[0][0].replace("-", ".")}\n'
                           f'<b>Всего записей в БД</b>: {metadata[0][2]}\n'
                           f'<b>Всего записей на сайте</b>: {metadata[0][1]}',
                  parse_mode='HTML')
